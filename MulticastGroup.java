daabb4c8009623d587dc1cd5ae52318e8f1228c6
KengoWada <louiskengo3@gmail.com>
2023-05-21 15:11:20 +0300

    Added extending base class

--- MulticastGroup.java
+++ MulticastGroup.java
@@ -5,15 +5,10 @@ import java.net.MulticastSocket;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class MulticastGroup implements Runnable {
-    public static final int PORT = 5000;
-    public static final String[] GROUP_ADDRESSES = { "226.4.5.6", "226.4.5.7", "226.4.5.8" };
-    public static final String[] GROUP_NAMES = { "timetable", "exams", "classes" };
+public class MulticastGroup extends BaseMulticastGroup implements Runnable {
 
     public static Scanner scanner = new Scanner(System.in);
 
-    public String groupName;
-    public String groupAddress;
     public String username;
 
     public MulticastGroup(String group, String name) {
@@ -113,11 +108,6 @@ public class MulticastGroup implements Runnable {
         }
 
         return errorMessage;
-    }
-
-    public String getGroupAddress(String groupName) {
-        int groupNameIndex = Arrays.asList(GROUP_NAMES).indexOf(groupName);
-        return GROUP_ADDRESSES[groupNameIndex];
     }
 
     public void run() {
